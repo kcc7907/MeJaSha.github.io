e053baabe1f95d7bd1a65d12c9b56503b4ec6a6e
kcc7907 <kcc7907@gmail.com>
2020-09-21 21:14:08 +0800

    0921.2114

--- js/common.js
+++ js/common.js
@@ -1,32 +1,100 @@
+let hidebg = document.querySelectorAll('.hidebg')[0];
+let hidebox = document.querySelectorAll('.hidebox')[0];
+let login2 = document.querySelectorAll('.login_2')[0];
+let signin1 = document.querySelectorAll('.signin1')[0];
+let flip = document.querySelectorAll('.flip')[0];
+let hmeau = document.querySelectorAll(".hide_meau")[0];
+let flipout = document.querySelectorAll(".flipout")[0];
+let signin_page = document.querySelectorAll(".signin_page")[0];
+let r_top_r = document.querySelectorAll(".r-top-r")[0];
+let r_top_l = document.querySelectorAll(".r-top-l")[0];
+let menu = document.querySelectorAll(".menu")[0];
+let hidelist = document.querySelectorAll(".hidelist")[0];
+let oderlist = document.querySelectorAll(".oderlist")[0];
+
 function show() {
-  let hideobj = document.querySelectorAll(".hidebg")[0];
-  hideobj.style.display = "block";
-  hideobj.style.height = document.body.clientHeight + "px";
-  document.getElementsByClassName("hidebox")[0].style.display = "block";
+  hidebg.style.display = "block";
+  hidebg.style.height = document.body.clientHeight + "px";
+  flipout.style.display = "block";
+  hidebox.classList.add('-on');
 }
 
 function hide() {
-  document.getElementsByClassName("hidebg")[0].style.display = "none";
-  document.getElementsByClassName("hidebox")[0].style.display = "none";
-}
-
-function signin() {
-  document.getElementsByClassName("signin_page")[0].style.display = "block";
-  document.getElementsByClassName("hidebox")[0].style.display = "none";
-}
-
-function login() {
-  document.getElementsByClassName("hidebox")[0].style.display = "block";
-  document.getElementsByClassName("signin_page")[0].style.display = "none";
-
-}
-let hmeau = document.querySelectorAll(".hide_meau")[0];
-
-function meau_hide() {
-
-  if (hmeau.style.display == "none") {
-    hmeau.style.display = 'block';
-  } else {
-    hmeau.style.display = 'none';
+  hidebg.style.display = "none";
+  flipout.style.display = "none";
+  if (flip.classList.contains('-on')) {
+    flip.classList.remove('-on');
   }
 }
+
+signin1.addEventListener('click', function () {
+  flip.classList.add('-on');
+});
+
+login2.addEventListener('click', function () {
+  flip.classList.remove('-on');
+});
+
+menu.addEventListener('click', function () {
+  hidelist.style.display = "block";
+  hidebg.style.height = document.body.clientHeight + "px";
+  hidebg.style.display = "block";
+});
+
+r_top_r.addEventListener('click', function () {
+  hidelist.style.display = "none";
+  hidebg.style.display = "none";
+});
+
+r_top_l.addEventListener('click', function () {
+  if (oderlist.style.display === "none" || oderlist.style.display === "") {
+    oderlist.style.display = "block";
+  } else {
+    oderlist.style.display = "none";
+  }
+});
+
+function meau_hide() {
+  if (hmeau.style.display == "none" || hmeau.style.display == "") {
+    hmeau.style.display = 'block';
+    hmeau.style.maxHeight = hmeau.scrollHeight + "px";
+  } else {
+    hmeau.style.maxHeight = '0';
+    setTimeout(function () {
+      hmeau.style.display = 'none';;
+    }, 1000);
+  }
+};
+
+
+window.onscroll = function () {
+  myFunction()
+};
+
+let qwe = document.querySelector("#calendar").scrollTop;
+let qaz = document.querySelector("#process").getBoundingClientRect().top + window.scrollY;
+let wsx = document.querySelector("#news").getBoundingClientRect().top + window.scrollY;
+let edc = document.querySelector("#calendar").getBoundingClientRect().top + window.scrollY;
+let edc1 = document.querySelector("#calendar").getBoundingClientRect().top;
+let rfv = document.querySelector("#gotop").getBoundingClientRect().top + window.scrollY;
+let iop = document.querySelector("#gotop");
+
+function myFunction() {
+  if (document.documentElement.scrollTop > (qaz - 300)) {
+    document.getElementById("process").classList.add('-on');
+  }
+  if (document.documentElement.scrollTop > (wsx - 300)) {
+    document.getElementById("news").classList.add('-on');
+  }
+  if (document.querySelector("#calendar").getBoundingClientRect().top < 300) {
+    document.getElementById("calendar").classList.add('-on');
+  }
+  iop.classList.toggle('-on', window.scrollY > (wsx - 300));
+}
+
+iop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+})
